1d9db0acdbcc12c11a45857b1dd0beab83e26bfb
raccoonboy <raccoonboy0803@gmail.com>
2024-04-27 22:25:45 +0900

    Fix: API key gitignore 처리

--- src/main.js
+++ src/main.js
@@ -1,11 +1,4 @@
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MjMyMzQ1MGUwMzU1NGJiMThiZGU5MzFiZTI0OGE0ZiIsInN1YiI6IjY2MmNhZjk0MDI4ZjE0MDEyMjY4NjVkMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sbxMx311toX_9SAmkYxUWENEO3z-gQK4GZhYmbUeXbg',
-  },
-};
+import options from './options.js';
 
 const listFieldElement = document.querySelector('.listField');
 const inputElement = document.querySelector('#searchInput');
