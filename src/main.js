7de84cda0036a83ef89ecb49857cfc939fe8bd01
raccoonboy <raccoonboy0803@gmail.com>
2024-04-30 14:48:10 +0900

    Fix: main.js 기능별 함수 분류

--- src/main.js
+++ src/main.js
@@ -6,81 +6,83 @@ const buttonElement = document.querySelector('.searchButton');
 const notFoundContainer = document.querySelector('.notFoundContainer');
 const returnBtn = document.querySelector('.returnBtn');
 
-function saveData(data) {
-  console.log(data);
+function fetchData() {
+  fetch(
+    'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1',
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const dataFormat = data.results;
+      populateDOM(dataFormat);
+      alertCardId();
+      searchMovieTitle();
+    })
+    .catch((error) => console.error(error));
 }
 
-async function fetchDataAndPopulateDOM() {
-  try {
-    const response = await fetch(
-      'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1',
-      options
-    );
+function populateDOM(data) {
+  const imgRoot = 'https://image.tmdb.org/t/p/w200';
+  let allCardHtml = ``;
 
-    const data = await response.json();
-    saveData(data);
-    const imgRoot = 'https://image.tmdb.org/t/p/w200';
+  data.forEach((item) => {
+    const tempHtml = `<div class="cardItem" data-id=${item.id}>
+    <img src="${imgRoot}${item.poster_path}" alt="movie_img" class="cardImg" />
+    <div class="cardDescription">
+      <h2 class="descTitle">${item.title}</h2>
+      <p class="descVoteAvg">rating : ${item.vote_average}</p>
+      <p class="descReleaseDate">release : ${item.release_date}</p>
+      <p class="descOverview">${item.overview}</p>
+    </div>
+  </div>`;
 
-    let allCardHtml = ``;
+    allCardHtml += tempHtml;
+  });
+  listFieldElement.innerHTML = allCardHtml;
+}
 
-    data.results.forEach((item) => {
-      const tempHtml = `<div class="cardItem" data-id=${item.id}>
-      <img src="${imgRoot}${item.poster_path}" alt="movie_img" class="cardImg" />
-      <div class="cardDescription">
-        <h2 class="descTitle">${item.title}</h2>
-        <p class="descVoteAvg">rating : ${item.vote_average}</p>
-        <p class="descReleaseDate">release : ${item.release_date}</p>
-        <p class="descOverview">${item.overview}</p>
-      </div>
-    </div>`;
-
-      allCardHtml += tempHtml;
+function alertCardId() {
+  const cardItemArray = [...listFieldElement.querySelectorAll('.cardItem')];
+  cardItemArray.forEach((item) => {
+    item.addEventListener('click', () => {
+      alert(`영화 id: ${item.dataset.id}`);
     });
+  });
+}
 
-    listFieldElement.innerHTML = allCardHtml;
+function searchMovieTitle() {
+  const elementArray = listFieldElement.querySelectorAll('.cardItem');
+  buttonElement.addEventListener('click', (event) => {
+    event.preventDefault();
+    const keyword = inputElement.value.toLowerCase();
 
-    const cardItemArray = [...listFieldElement.querySelectorAll('.cardItem')];
-    cardItemArray.forEach((item) => {
-      item.addEventListener('click', () => {
-        alert(`영화 id: ${item.dataset.id}`);
-      });
-    });
+    elementArray.forEach((_, index) => {
+      const movieTitle = elementArray[index]
+        .querySelector('div')
+        .querySelector('.descTitle')
+        .innerText.toLowerCase();
 
-    const elementArray = listFieldElement.querySelectorAll('.cardItem');
-
-    buttonElement.addEventListener('click', (event) => {
-      event.preventDefault();
-      const keyword = inputElement.value.toLowerCase();
-
-      elementArray.forEach((_, index) => {
-        const movieTitle = elementArray[index]
-          .querySelector('div')
-          .querySelector('.descTitle')
-          .innerText.toLowerCase();
-
-        if (!movieTitle.includes(keyword)) {
-          elementArray[index].classList.add('hidden');
-        } else {
-          elementArray[index].classList.remove('hidden');
-        }
-      });
-
-      const result = Array.from(elementArray).every((item) => {
-        return item.className.includes('hidden');
-      });
-
-      if (result) {
-        notFoundContainer.classList.remove('hidden');
+      if (!movieTitle.includes(keyword)) {
+        elementArray[index].classList.add('hidden');
       } else {
-        notFoundContainer.classList.add('hidden');
+        elementArray[index].classList.remove('hidden');
       }
     });
-  } catch (error) {
-    console.error(error);
-  }
+
+    const result = Array.from(elementArray).every((item) => {
+      return item.className.includes('hidden');
+    });
+
+    if (result) {
+      notFoundContainer.classList.remove('hidden');
+    } else {
+      notFoundContainer.classList.add('hidden');
+    }
+  });
 }
 
-window.addEventListener('DOMContentLoaded', fetchDataAndPopulateDOM);
+fetchData();
+
 window.addEventListener('load', () => inputElement.focus());
 
 const originalPlaceholder = inputElement.placeholder;
@@ -90,7 +92,6 @@ inputElement.addEventListener('focus', function () {
 inputElement.addEventListener('blur', function () {
   this.placeholder = originalPlaceholder;
 });
-
 returnBtn.addEventListener('click', () => {
   window.location.reload();
 });
